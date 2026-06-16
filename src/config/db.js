93ce03f1df869702f34457e4373c04197ffc5469
Pankaj kumar Jha <pankajjha9630@gmail.com>
2026-06-17 02:12:07 +0530

    Fix environment variable name for MySQL connection in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  uri: process.env.MYSQL_URL,
+  uri: process.env.MYSQL_PUBLIC_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
