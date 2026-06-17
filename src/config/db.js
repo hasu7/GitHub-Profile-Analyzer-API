869554d09c3f44b0fabb415f7b1c8adbb0f6ec8d
Pankaj kumar Jha <pankajjha9630@gmail.com>
2026-06-17 11:44:59 +0530

    Update database connection URI and configure SSL settings in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,11 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  uri: process.env.MYSQL_PUBLIC_URL,
+  uri: process.env.SERVICE_URI,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  enableKeepAlive: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
